f7ee408d94b0346c26d7095525813d65d28bf72e
tamarochka <tamara.zenkevich@gmail.com>
2015-01-02 09:47:50 -0500

    test with valid attr pass

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,4 +1,26 @@
 class ManufacturersController < ApplicationController
   def new
+    @manufacturer = Manufacturer.new
   end
+
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+    if @manufacturer.save
+      redirect_to manufacturers_path, notice: "New manufacturer was successfully added"
+    else
+      render :new
+    end
+  end
+
+  def index
+    @manufacturers = Manufacturer.all
+  end
+
+
+  private
+
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name, :country)
+  end
+
 end
