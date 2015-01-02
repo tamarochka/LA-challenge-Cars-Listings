c3c34468e9f03e7feca4f5490955cc2300d25aaf
tamarochka <tamara.zenkevich@gmail.com>
2015-01-02 09:26:40 -0500

    added Manufacturers table and model

---
+++ db/migrate/20150102142334_create_manufacturer_table.rb
@@ -0,0 +1,10 @@
+class CreateManufacturerTable < ActiveRecord::Migration
+  def change
+    create_table :manufacturers do |t|
+      t.string :name, null: false
+      t.string :country, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150102140904) do
+ActiveRecord::Schema.define(version: 20150102142334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "manufacturers", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "country",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
